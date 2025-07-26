fb0961f0672f3c59c1f7ca581d7e82a2e33287ab
Alemilson Silva <alemilson.silva@estudate.ufjf.br>
2025-07-26 14:34:59 -0300

    Criação da funcionalidade para limpar lista

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-import { getLista } from "./lista"
+import { getLista, limparLista } from "./lista.js";
 
-const olItens = document.querySelector('#itens')
-const pEntrada = document.querySelector('#entrada')
-const btnAdicionar = document.querySelector('#adicionar')
-const btnLimpar = document.querySelector('#limpar')
+const olItens = document.querySelector('#itens');
+const pEntrada = document.querySelector('#entrada');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
 
+atualizarLista();
 
 function atualizarLista(){
     olItens.innerHTML = ''
     let lista = getLista()
+    console.log(lista);
     for(let i = 0; i < lista.length; i++){
         const li = document.createElement('li');
         li.textContent = lista[i];
         olItens.appendChild(li);
     }
+}
+
+btnLimpar.addEventListener('click', limparitensDeLista);
+
+function limparitensDeLista(){
+    limparLista();
+    atualizarLista();
 }
